207ea16027000ed53f3f7798b8078e68b62a0ff9
Bhavesh Sooka <bsooka@gmail.com>
2024-09-03 21:54:25 +0200

    some explanationy stuff

--- haskell/app/AOC2015/Day01.hs
+++ haskell/app/AOC2015/Day01.hs
@@ -45,20 +45,27 @@ part2 = findFirstTimeInBasement 0 0 input
 
 -- Alternate solutions using scanl and takewhile
 part1_1 :: Int
-part1_1 = last 
-    $ scanl (+) 0 
-    $ calcNextFloor <$> input
+part1_1 = last                  -- [0, 1, 0, 1, 0, -1] -> -1
+    $ scanl (+) 0               -- [1, -1, 1, -1, -1] -> [0, 1, 0, 1, 0, -1]
+    $ calcNextFloor <$> input   -- "()())" -> [1, -1, 1, -1, -1]
 
 part2_1 :: Int
-part2_1 = length
-    $ takeWhile (/= (-1))
-    $ scanl (+) 0 
-    $ calcNextFloor <$> input
+part2_1 = length                -- [0, 1, 0, 1, 0] -> 5
+    $ takeWhile (/= (-1))       -- [0, 1, 0, 1, 0, -1] -> [0, 1, 0, 1, 0]
+    $ scanl (+) 0               -- [1, -1, 1, -1, -1] -> [0, 1, 0, 1, 0, -1]
+    $ calcNextFloor <$> input   -- "()())" -> [1, -1, 1, -1, -1]
 
 -- Alternate solutions using fold
 part1_2 :: Int
 part1_2 = foldl (+) 0 
     $ calcNextFloor <$> input
+
+--foldlWhile :: (\currentCount _newFloor -> currentCount + 1) 
+--              -> (\currentFloor instruction -> currentFloor + instruction)
+--              -> (\newFloor -> newFloor == -1) 
+--              -> (newCount, newFloor) 
+--              -> (1 : [-1, 1, -1, etc...])
+--              -> b
 
 foldlWhile :: (b -> a -> b) -> (c -> a -> c) -> (c -> Bool) -> (b, c) -> [a] -> b
 foldlWhile _ _ _ (acc, _) [] = acc
