a9e53b77e1e7437e8eeb8c32653b5e2c7955712e
Bhavesh Sooka <bsooka@gmail.com>
2024-09-04 22:22:12 +0200

    2015 day 5 part 1

--- haskell/app/AOC2015/Module.hs
+++ haskell/app/AOC2015/Module.hs
@@ -7,6 +7,7 @@ import AOC2015.Day01 (printAoC2015Day01Answer)
 import AOC2015.Day02 (printAoC2015Day02Answer)
 import AOC2015.Day03 (printAoC2015Day03Answer)
 import AOC2015.Day04 (printAoC2015Day04Answer)
+import AOC2015.Day05 (printAoC2015Day05Answer)
 
 printAoC2015 :: IO ()
 printAoC2015 = do
@@ -14,3 +15,4 @@ printAoC2015 = do
   printAoC2015Day02Answer
   printAoC2015Day03Answer
   printAoC2015Day04Answer
+  printAoC2015Day05Answer
