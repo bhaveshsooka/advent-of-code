9af00771b59d8bc70ecbc1bb6548512892a7c607
Bhavesh Sooka <bsooka@gmail.com>
2024-09-04 20:21:26 +0200

    2015 day 4 part 1 and 2

--- haskell/app/AOC2015/Module.hs
+++ haskell/app/AOC2015/Module.hs
@@ -6,9 +6,11 @@ where
 import AOC2015.Day01 (printAoC2015Day01Answer)
 import AOC2015.Day02 (printAoC2015Day02Answer)
 import AOC2015.Day03 (printAoC2015Day03Answer)
+import AOC2015.Day04 (printAoC2015Day04Answer)
 
 printAoC2015 :: IO ()
 printAoC2015 = do
   printAoC2015Day01Answer
   printAoC2015Day02Answer
   printAoC2015Day03Answer
+  printAoC2015Day04Answer
