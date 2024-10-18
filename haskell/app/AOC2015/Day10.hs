37504395b02085c933d9575ffdbebc3e361c17fc
Bhavesh Sooka <bsooka@gmail.com>
2024-10-18 18:42:11 +0200

    added helper for day 10 & 7

--- haskell/app/AOC2015/Day10.hs
+++ haskell/app/AOC2015/Day10.hs
@@ -1,11 +1,18 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module AOC2015.Day10 (
-  printAoC2015Day10Answer,
+  parts,
 ) where
 
 import Data.Text qualified as T
-import Data.Text.IO qualified as TIO
+import Util.AOCHelpers (Part (Part), Parts)
+
+parts :: Parts
+parts = (Part part1, Part part2)
+
+part1 :: T.Text -> Int
+part1 input = T.length $ repeatDescribe 40 input
+
+part2 :: T.Text -> Int
+part2 input = T.length $ repeatDescribe 50 input
 
 describeNumber :: T.Text -> T.Text
 describeNumber s = T.concat $ transform <$> T.group s
@@ -16,13 +23,3 @@ transform s = (T.pack $ show $ T.length s) <> T.take 1 s
 repeatDescribe :: Int -> T.Text -> T.Text
 repeatDescribe 0 s = s
 repeatDescribe n s = repeatDescribe (n - 1) $ describeNumber s
-
-printAoC2015Day10Answer :: IO ()
-printAoC2015Day10Answer = do
-  input <- TIO.readFile "./data/2015-10.txt"
-  putStrLn "------ Day 10 ------"
-  let part1 = T.length $ repeatDescribe 40 input
-  putStrLn $ "part1: " ++ show part1
-  let part2 = T.length $ repeatDescribe 50 input
-  putStrLn $ "part2: " ++ show part2
-  putStrLn ""
--- haskell/app/AOC2015/Module.hs
+++ haskell/app/AOC2015/Module.hs
@@ -9,10 +9,10 @@ import AOC2015.Day03 qualified as Day03
 import AOC2015.Day04 qualified as Day04
 import AOC2015.Day05 qualified as Day05
 import AOC2015.Day06 qualified as Day06
-import AOC2015.Day07GPT (printAoC2015Day07GPTAnswer)
+import AOC2015.Day07GPT qualified as Day07
 import AOC2015.Day08 qualified as Day08
 import AOC2015.Day09 qualified as Day09
-import AOC2015.Day10 (printAoC2015Day10Answer)
+import AOC2015.Day10 qualified as Day10
 import Util.AOCHelpers (printDay)
 
 year :: Int
@@ -20,13 +20,13 @@ year = 2015
 
 printAoC2015 :: IO ()
 printAoC2015 = do
-  printDay (year, 1) Day01.parts
-  printDay (year, 2) Day02.parts
-  printDay (year, 3) Day03.parts
-  printDay (year, 4) Day04.parts
-  printDay (year, 5) Day05.parts
-  printDay (year, 6) Day06.parts
-  printAoC2015Day07GPTAnswer
-  printDay (year, 8) Day08.parts
-  printDay (year, 9) Day09.parts
-  printAoC2015Day10Answer
+  printDay (year, 01) Day01.parts
+  printDay (year, 02) Day02.parts
+  printDay (year, 03) Day03.parts
+  printDay (year, 04) Day04.parts
+  printDay (year, 05) Day05.parts
+  printDay (year, 06) Day06.parts
+  printDay (year, 07) Day07.parts
+  printDay (year, 08) Day08.parts
+  printDay (year, 09) Day09.parts
+  printDay (year, 10) Day10.parts
