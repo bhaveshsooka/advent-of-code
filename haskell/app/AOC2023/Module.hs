6b05d1791f3e40029d520667bf225f9248915bea
Bhavesh Sooka <bsooka@gmail.com>
2024-10-19 13:48:15 +0200

    helper for 2023 day 1

--- haskell/app/AOC2023/Module.hs
+++ haskell/app/AOC2023/Module.hs
@@ -3,8 +3,12 @@ module AOC2023.Module (
 )
 where
 
-import AOC2023.Day01
+import AOC2023.Day01 qualified as Day01
+import Util.AOCHelpers (printDay)
+
+year :: Int
+year = 2023
 
 printAoC2023 :: IO ()
 printAoC2023 = do
-  printAoC2023Day01Answer
+  printDay (year, 01) Day01.parts
