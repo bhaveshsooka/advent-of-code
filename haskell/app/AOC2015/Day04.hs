bba5d4a4719fc5069e596440f52490668a929dcf
Bhavesh Sooka <bsooka@gmail.com>
2024-10-21 10:24:26 +0200

    some playing with day4

--- haskell/app/AOC2015/Day04.hs
+++ haskell/app/AOC2015/Day04.hs
@@ -4,7 +4,7 @@ module AOC2015.Day04 (
 
 import Crypto.Hash.MD5 (hash)
 import Data.ByteString.Base16 (encode)
-import Data.ByteString.Char8 (ByteString, pack, take)
+import Data.ByteString.Char8 (ByteString, isPrefixOf, pack, take)
 import Data.Text qualified as T
 import Data.Text.Encoding (encodeUtf8)
 import Util.AOCHelpers (Part (Part), Parts)
@@ -14,15 +14,15 @@ parts :: Parts
 parts = (Part part1, Part part2)
 
 part1 :: T.Text -> Int
-part1 input = findNum (encodeUtf8 input) (encodeUtf8 input) 5 0
+part1 input = findNum (encodeUtf8 input) (pack (replicate 5 '0')) 0
 
 part2 :: T.Text -> Int
-part2 input = findNum (encodeUtf8 input) (encodeUtf8 input) 6 (part1 input)
+part2 input = findNum (encodeUtf8 input) (pack (replicate 6 '0')) (part1 input)
 
-findNum :: ByteString -> ByteString -> Int -> Int -> Int
-findNum input h count num =
-  if take count h == pack (replicate count '0')
+findNum :: ByteString -> ByteString -> Int -> Int
+findNum input prefix num =
+  if isPrefixOf prefix md5Hash
     then num
-    else findNum input (encode . hash $ newInput) count (num + 1)
+    else findNum input prefix $ num + 1
  where
-  newInput = input <> (pack . show $ num + 1)
+  md5Hash = encode . hash $ input <> (pack . show $ num)
