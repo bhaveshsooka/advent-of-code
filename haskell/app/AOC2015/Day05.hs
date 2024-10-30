3da0fea1cc06ae591e0bf347eefed0a90f2dd682
Bhavesh Sooka <bsooka@gmail.com>
2024-10-30 13:47:00 +0200

    2015 day 5 part 2

--- haskell/app/AOC2015/Day05.hs
+++ haskell/app/AOC2015/Day05.hs
@@ -10,15 +10,16 @@ part1 input = length $ filter predicate inputLines
  where
   inputLines = T.lines input
   predicate l = atleastThreeVowels l && containsDoubleLetter l && doesNotContainBadStrings l
+  atleastThreeVowels = (>= 3) . T.length . T.filter (`elem` "aeiou")
+  containsDoubleLetter = any ((> 1) . T.length) . T.group
+  doesNotContainBadStrings x = not $ any ((`T.isInfixOf` x) . T.pack) ["ab", "cd", "pq", "xy"]
 
-part2 :: T.Text -> String
-part2 _input = "Not there yet"
-
-atleastThreeVowels :: T.Text -> Bool
-atleastThreeVowels = (>= 3) . T.length . T.filter (`elem` "aeiou")
-
-containsDoubleLetter :: T.Text -> Bool
-containsDoubleLetter = any ((> 1) . T.length) . T.group
-
-doesNotContainBadStrings :: T.Text -> Bool
-doesNotContainBadStrings x = not $ any ((`T.isInfixOf` x) . T.pack) ["ab", "cd", "pq", "xy"]
+part2 :: T.Text -> Int
+part2 input = length $ filter predicate inputLines
+ where
+  inputLines = T.lines input
+  predicate l = hasRepeatedPair (T.unpack l) && hasRepeatingLetterWithGap (T.unpack l)
+  hasRepeatedPair s = any (\i -> take 2 (drop i s) `elem` pairs (i + 2)) [0 .. length s - 2]
+    where
+      pairs start = [take 2 (drop j s) | j <- [start .. length s - 2]]
+  hasRepeatingLetterWithGap s = any (\i -> s !! i == s !! (i + 2)) [0 .. length s - 3]
