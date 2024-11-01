f54c5b69761872fbba19e4f10d9e40337e98b8d9
Bhavesh Sooka <bsooka@gmail.com>
2024-11-01 16:26:20 +0200

    2015 day 2 refactor

--- haskell/app/AOC2015/Day02.hs
+++ haskell/app/AOC2015/Day02.hs
@@ -13,36 +13,25 @@ part1 input = sum $ zipWith (+) presentSurfaceAreas smallestSides
  where
   presentSurfaceAreas = surfaceArea <$> parseDimensionsList input
   smallestSides = smallestSide <$> parseDimensionsList input
+  surfaceArea (Box l w h) = 2 * (area l w + area w h + area l h)
+  smallestSide (Box l w h) = minimum [area l w, area w h, area h l]
 
 part2 :: T.Text -> Int
 part2 input = sum $ zipWith (+) wrapRibbonRequired bowRibbonRequired
  where
   wrapRibbonRequired = ribbonForWrap <$> parseDimensionsList input
   bowRibbonRequired = ribbonForBow <$> parseDimensionsList input
+  ribbonForBow (Box l w h) = l * w * h
+  ribbonForWrap (Box l w h) = (2 * minimum [l, w, h]) + (2 * sort [l, w, h] !! 1)
 
-type PresentDimension = (Int, Int, Int)
+data PresentDimension = Box Int Int Int
 
 parseDimensionsList :: T.Text -> [PresentDimension]
 parseDimensionsList input = parseAoCInput input dimensionsListParser "dimensionsListParser"
  where
-  dimensionsParser = do
-    l <- P.many1 P.digit <* P.char 'x'
-    w <- P.many1 P.digit <* P.char 'x'
-    h <- P.many1 P.digit
-    pure $ (read l, read w, read h)
+  numParser = read <$> P.many1 P.digit
+  dimensionsParser = Box <$> numParser <* P.char 'x' <*> numParser <* P.char 'x' <*> numParser
   dimensionsListParser = P.many1 $ dimensionsParser <* P.optional P.newline
 
 area :: Int -> Int -> Int
 area l w = l * w
-
-surfaceArea :: PresentDimension -> Int
-surfaceArea (l, w, h) = 2 * (area l w + area w h + area l h)
-
-smallestSide :: PresentDimension -> Int
-smallestSide (l, w, h) = min (area l w) (min (area w h) (area h l))
-
-ribbonForBow :: PresentDimension -> Int
-ribbonForBow (l, w, h) = l * w * h
-
-ribbonForWrap :: PresentDimension -> Int
-ribbonForWrap (l, w, h) = (2 * sort [l, w, h] !! 0) + (2 * sort [l, w, h] !! 1)
