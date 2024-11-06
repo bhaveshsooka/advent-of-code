beebfd460e75c70fbdaba2792ca29dffb2fe0ef1
Bhavesh Sooka <bsooka@gmail.com>
2024-11-06 10:54:31 +0200

    refactor day 3

--- haskell/app/AOC2015/Day03.hs
+++ haskell/app/AOC2015/Day03.hs
@@ -3,20 +3,18 @@ module AOC2015.Day03 (
   part2,
 ) where
 
-import Data.List (nub)
+import Data.List (nub, partition)
 import Data.Text qualified as T
 
 part1 :: T.Text -> Int
-part1 input = length $ nub $ scanAndTake 1 allCoords
- where
-  allCoords = getCoords input
+part1 input = length . nub . scanl (<>) mempty $ getCoord <$> T.unpack input
 
 part2 :: T.Text -> Int
-part2 input = length $ nub $ santa ++ roboSanta
+part2 input = length . nub $ santa <> roboSanta
  where
-  allCoords = getCoords input
-  santa = scanAndTake 2 allCoords
-  roboSanta = scanAndTake 2 $ mempty : allCoords
+  xx = partition (\(i :: Int, _) -> even i) $ zip [0 ..] $ getCoord <$> T.unpack input
+  santa = scanl (<>) mempty $ snd <$> fst xx
+  roboSanta = scanl (<>) mempty $ snd <$> snd xx
 
 data Coord = Coord Int Int deriving (Show, Eq)
 
@@ -28,9 +26,6 @@ instance Monoid Coord where
   mempty :: Coord
   mempty = Coord 0 0
 
-scanAndTake :: (Monoid a) => Int -> [a] -> [a]
-scanAndTake n = scanl (<>) mempty . takeEvery n
-
 getCoord :: Char -> Coord
 getCoord c = case c of
   '^' -> Coord 0 1
@@ -38,17 +33,3 @@ getCoord c = case c of
   '<' -> Coord (-1) 0
   '>' -> Coord 1 0
   _ -> error "Invalid direction"
-
-getCoords :: T.Text -> [Coord]
-getCoords input = getCoord <$> T.unpack input
-
-takeEvery :: Int -> [a] -> [a]
-takeEvery step xs = compute validated
- where
-  compute ys = case drop (step - 1) ys of
-    [] -> []
-    y : ys' -> y : compute ys'
-  validated =
-    if step > 0
-      then xs
-      else error "Data.List.Transform.takeEvery: Step parameter must be positive."
