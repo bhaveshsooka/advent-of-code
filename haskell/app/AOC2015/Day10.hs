74131657e6fde8607288e433c30b2d86ebf1cac2
Bhavesh Sooka <bsooka@gmail.com>
2024-11-11 21:52:01 +0200

    refactor day 10

--- haskell/app/AOC2015/Day10.hs
+++ haskell/app/AOC2015/Day10.hs
@@ -12,12 +12,8 @@ part1 input = T.length $ repeatDescribe 40 input
 part2 :: T.Text -> Int
 part2 input = T.length $ repeatDescribe 50 input
 
-describeNumber :: T.Text -> T.Text
-describeNumber s = T.concat $ transform <$> T.group s
-
-transform :: T.Text -> T.Text
-transform s = T.pack (show $ T.length s) <> T.take 1 s
-
 repeatDescribe :: Int -> T.Text -> T.Text
-repeatDescribe 0 s = s
-repeatDescribe n s = repeatDescribe (n - 1) $ describeNumber s
+repeatDescribe n s = if n == 0 then s else repeatDescribe (n - 1) $ describeNumber s
+  where
+    describeNumber s' = T.concat $ transform <$> T.group s'
+    transform s' = T.pack (show $ T.length s') <> T.take 1 s'
