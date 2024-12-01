935f5362c0b3761f30b4a17c0bc5ca76d1d336d5
Bhavesh Sooka <bsooka@gmail.com>
2024-12-01 09:02:56 +0200

    use Bifunctor to sort

--- haskell/app/AOC2024/Day01.hs
+++ haskell/app/AOC2024/Day01.hs
@@ -4,25 +4,23 @@ module AOC2024.Day01
   )
 where
 
+import Data.Bifunctor qualified
 import Data.List (sort)
 import Data.Text qualified as T
-import Text.Parsec qualified as P
 
 part1 :: T.Text -> Int
 part1 input = sum $ zipWith (\x y -> abs (x - y)) one two
   where
-    one = sort . fst $ parseLocationPairs input
-    two = sort . snd $ parseLocationPairs input
+    (one, two) = parseLocationPairs input
 
--- part2 :: T.Text -> Int
+part2 :: T.Text -> Int
 part2 input = sum $ (\x -> occurrance x two * x) <$> one
   where
-    one = sort . fst $ parseLocationPairs input
-    two = sort . snd $ parseLocationPairs input
+    (one, two) = parseLocationPairs input
     occurrance a = length . filter (a ==)
 
 parseLocationPairs :: T.Text -> ([Int], [Int])
-parseLocationPairs input = unzip $ xx <$> T.lines input
+parseLocationPairs input = Data.Bifunctor.bimap sort sort . unzip $ createPairs <$> T.lines input
   where
     splitLine l = read . T.unpack <$> T.splitOn (T.pack "   ") l
-    xx l = (head (splitLine l), last (splitLine l))
+    createPairs l = (head (splitLine l), last (splitLine l))
