96172eeb063fc9f96e450cd7c988a0c6594eccc6
Bhavesh Sooka <bsooka@gmail.com>
2024-12-25 18:07:04 +0200

    day 23 part 1

--- haskell/app/AOC2024/Module.hs
+++ haskell/app/AOC2024/Module.hs
@@ -15,6 +15,7 @@ import AOC2024.Day09 qualified as Day09
 import AOC2024.Day10 qualified as Day10
 import AOC2024.Day11 qualified as Day11
 import AOC2024.Day14 qualified as Day14
+import AOC2024.Day23 qualified as Day23
 import AOC2024.Day24 qualified as Day24
 import Model (Part (Part), Parts, errMsgParts)
 
@@ -33,6 +34,7 @@ getParts day =
     10 -> (Part Day10.part1, Part Day10.part2)
     11 -> (Part Day11.part1, Part Day11.part2)
     14 -> (Part Day14.part1, Part Day14.part2)
+    23 -> (Part Day23.part1, Part Day23.part2)
     24 -> (Part Day24.part1, Part Day24.part2)
     _ -> errMsgParts errMsg
   where
