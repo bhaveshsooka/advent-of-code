c2f6875f4e91ca11180ee5f28fdd4e3e64301c5a
Bhavesh Sooka <bsooka@gmail.com>
2024-12-31 08:00:37 +0200

    some refactoring

--- haskell/app/AOC2023/Day01.hs
+++ haskell/app/AOC2023/Day01.hs
@@ -1,47 +1,38 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module AOC2023.Day01
   ( part1,
     part2,
   )
 where
 
+import Data.Char (isDigit)
 import Data.Text qualified as T
 
 part1 :: T.Text -> Int
-part1 input = sum $ read <$> list
+part1 input = sum numbersList
   where
-    list = extractFirstAndLast . numericString <$> T.lines input
+    numbersList = read . extractFirstAndLast <$> T.lines input
 
 part2 :: T.Text -> Int
-part2 input = sum $ read <$> list
+part2 input = sum numbersList
   where
-    list = (extractFirstAndLast <$> numericString) . replaceWordsWithDigits <$> T.lines input
+    numbersList = read . extractFirstAndLast . replaceWordsWithDigits <$> T.lines input
 
 replaceWordsWithDigits :: T.Text -> T.Text
-replaceWordsWithDigits str =
-  replace "one" "o1e" $
-    replace "two" "t2o" $
-      replace "three" "t3e" $
-        replace "four" "f4r" $
-          replace "five" "f5e" $
-            replace "six" "s6x" $
-              replace "seven" "s7n" $
-                replace "eight" "e8t" $
-                  replace "nine" "n9e" str
+replaceWordsWithDigits str = foldr (\(old, new) s -> T.replace old new s) str replacements
+  where
+    replacements =
+      [ (T.pack "one", T.pack "o1e"),
+        (T.pack "two", T.pack "t2o"),
+        (T.pack "three", T.pack "t3e"),
+        (T.pack "four", T.pack "f4r"),
+        (T.pack "five", T.pack "f5e"),
+        (T.pack "six", T.pack "s6x"),
+        (T.pack "seven", T.pack "s7n"),
+        (T.pack "eight", T.pack "e8t"),
+        (T.pack "nine", T.pack "n9e")
+      ]
 
--- Helper function to replace all instances of a substring
-replace :: T.Text -> T.Text -> T.Text -> T.Text
-replace old new = T.intercalate new . T.splitOn old
-
-numericString :: T.Text -> T.Text
-numericString = T.filter (\c -> c `elem` ['1' .. '9'])
-
-f :: T.Text -> Char
-f = T.head . numericString
-
-l :: T.Text -> Char
-l = T.last . numericString
-
-extractFirstAndLast :: T.Text -> [Char]
-extractFirstAndLast s = f s : [l s]
+extractFirstAndLast :: T.Text -> String
+extractFirstAndLast str = T.unpack $ T.take 1 s <> T.drop (T.length s - 1) s
+  where
+    s = T.filter isDigit str
