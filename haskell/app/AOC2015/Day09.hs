f34f682ee70f0feafaffeba4111c95712398ed31
Bhavesh Sooka <bsooka@gmail.com>
2025-01-28 22:34:15 +0200

    a small refactor for readability

--- haskell/app/AOC2015/Day09.hs
+++ haskell/app/AOC2015/Day09.hs
@@ -4,7 +4,7 @@ module AOC2015.Day09
   )
 where
 
-import Data.Foldable (minimumBy, maximumBy)
+import Data.Foldable (maximumBy, minimumBy)
 import Data.Function (on)
 import Data.HashMap.Strict qualified as M
 import Data.Maybe (mapMaybe)
@@ -14,50 +14,63 @@ import Text.Parsec qualified as P
 import Util.ParseHelpers (parseAoCInput)
 
 part1 :: T.Text -> Int
-part1 input = fst $ minimumBy (compare `on` fst) $ filter ((== numCities) . length . snd) allPaths
+part1 input = fst $ minimumBy (compare `on` fst) fullPaths
   where
-    allPaths = concat $ foldr (\(c1, c2) acc -> findPaths routes c1 c2 : acc) [] cityPairs
-    cityPairs = [(c1, c2) | c1 <- M.keys routes, c2 <- M.keys routes]
-    numCities = length $ M.keys routes
-    routes = parseRoutes input
+    fullPaths = filter ((== length cities) . length . snd) allPaths
+    allPaths =
+      concat
+        [ findPaths graph S.empty (0, [c1]) (c1, c2)
+          | c1 <- cities,
+            c2 <- cities,
+            c1 /= c2
+        ]
+    cities = M.keys graph
+    graph = parseRoutes input
 
 part2 :: T.Text -> Int
-part2 input = fst $ maximumBy (compare `on` fst) $ filter ((== numCities) . length . snd) allPaths
+part2 input = fst $ maximumBy (compare `on` fst) fullPaths
   where
-    allPaths = concat $ foldr (\(c1, c2) acc -> findPaths routes c1 c2 : acc) [] cityPairs
-    cityPairs = [(c1, c2) | c1 <- M.keys routes, c2 <- M.keys routes]
-    numCities = length $ M.keys routes
-    routes = parseRoutes input
+    fullPaths = filter ((== length cities) . length . snd) allPaths
+    allPaths =
+      concat
+        [ findPaths graph S.empty (0, [c1]) (c1, c2)
+          | c1 <- cities,
+            c2 <- cities,
+            c1 /= c2
+        ]
+    cities = M.keys graph
+    graph = parseRoutes input
 
-type Routes = M.HashMap String [(String, Int)]
+type City = String
 
-type Path = (Int, [String])
+type Distance = Int
 
-findPaths :: Routes -> String -> String -> [Path]
-findPaths routes start end = go S.empty (0, []) start
+type Graph = M.HashMap City [(City, Distance)]
+
+type Path = (Distance, [City])
+
+type Visited = S.Set City
+
+findPaths :: Graph -> Visited -> Path -> (City, City) -> [Path]
+findPaths graph visited (dist, route) (node, end)
+  | node == end = [(dist, route)]
+  | otherwise = concat $ mapMaybe buildPaths neighbors
   where
-    go visited (distance, p) current
-      | current == end = [(distance, current : p)]
-      | otherwise = concat $ mapMaybe buildPaths neighbors
+    neighbors = graph M.! node
+    buildPaths (neighbour, neighborDistance)
+      | neighbour `S.member` visited = Nothing
+      | otherwise = Just $ findPaths graph newVisited newPath (neighbour, end)
       where
-        neighbors = routes M.! current
-        buildPaths (next, distToNext)
-          | next `S.member` visited = Nothing
-          | otherwise = Just $ go (S.insert current visited) (distance + distToNext, current : p) next
+        newVisited = S.insert node visited
+        newPath = (dist + neighborDistance, node : route)
 
-parseRoutes :: T.Text -> Routes
+parseRoutes :: T.Text -> Graph
 parseRoutes input = foldr addRouteFold M.empty routes
   where
     numParser = read <$> P.many1 P.digit
-    cityParser = P.many1 P.letter
-    routeParser =
-      (,,)
-        <$> cityParser
-        <* P.string " to "
-        <*> cityParser
-        <* P.string " = "
-        <*> numParser
+    cityParser toVoid = P.many1 P.letter <* P.string toVoid
+    routeParser = (,,) <$> cityParser " to " <*> cityParser " = " <*> numParser
     routesParser = P.many1 $ routeParser <* P.optional P.newline
     routes = parseAoCInput input routesParser "routesParser"
-    putInMap (c1, c2, d) rs = M.insertWith (++) c1 [(c2, d)] (M.insertWith (++) c2 [(c1, d)] rs)
-    addRouteFold (c1, c2, d) = putInMap (c1, c2, d)
+    putInMap (c1, c2, d) = M.insertWith (++) c1 [(c2, d)]
+    addRouteFold (c1, c2, d) = putInMap (c1, c2, d) . putInMap (c2, c1, d)
