c7e7e93c18c9e8832f0280df6b3ce534afde359c
Bhavesh Sooka <bsooka@gmail.com>
2025-08-03 22:25:51 +0200

    2015-6: switch data structure for performance

--- haskell/app/AOC2015/Day06.hs
+++ haskell/app/AOC2015/Day06.hs
@@ -5,22 +5,35 @@ module AOC2015.Day06
 where
 
 import Data.Text qualified as T
-import Data.Vector.Unboxed qualified as V
+import Data.Vector.Unboxed.Mutable qualified as MV
 import Text.Parsec qualified as P
 import Util.GridUtils.Coord (Block, Coord (..))
-import Util.GridUtils.Grid (UnboxedGrid)
 import Util.ParseHelpers (parseAoCInput)
+import Control.Monad (forM_)
+import GHC.IO (unsafePerformIO)
+import qualified Data.Vector.Unboxed as V
 
 part1 :: T.Text -> Int
-part1 input = V.sum $ foldl (processInstruction False) emptyGrid $ parseInstructions input
+part1 input = V.sum finalGrid
+  where
+    instructions = parseInstructions input
+    gridSize = 1000
+    finalGrid = unsafePerformIO $ do
+      grid <- MV.replicate (gridSize * gridSize) 0 :: IO (MV.IOVector Int)
+      mapM_ (processInstruction False grid gridSize) instructions
+      V.freeze grid
 
 part2 :: T.Text -> Int
-part2 input = V.sum $ foldl (processInstruction True) emptyGrid $ parseInstructions input
+part2 input = V.sum finalGrid
+  where
+    instructions = parseInstructions input
+    gridSize = 1000
+    finalGrid = unsafePerformIO $ do
+      grid <- MV.replicate (gridSize * gridSize) 0 :: IO (MV.IOVector Int)
+      mapM_ (processInstruction True grid gridSize) instructions
+      V.freeze grid
 
 data Instruction = TurnOn Block | TurnOff Block | Toggle Block deriving (Show, Eq)
-
-emptyGrid :: UnboxedGrid Int
-emptyGrid = V.generate (1000 * 1000) $ const 0
 
 parseInstructions :: T.Text -> [Instruction]
 parseInstructions input = parseAoCInput input instructionsParser "instructionsParser"
@@ -34,15 +47,19 @@ parseInstructions input = parseAoCInput input instructionsParser "instructionsPa
     instructionParser = P.choice $ P.try <$> [turnOnParser, turnOffParser, toggleParser]
     instructionsParser = P.many1 $ instructionParser <* P.optional P.newline
 
-processInstruction :: Bool -> UnboxedGrid Int -> Instruction -> UnboxedGrid Int
-processInstruction b grid i =
+processInstruction :: Bool -> MV.IOVector Int -> Int -> Instruction -> IO ()
+processInstruction b grid numCols i = do
   case (i, b) of
-    (TurnOn block, False) -> grid V.// newVals block (const 1)
-    (TurnOff block, False) -> grid V.// newVals block (const 0)
-    (Toggle block, False) -> grid V.// newVals block (\v -> if v == 0 then 1 else 0)
-    (TurnOn block, True) -> grid V.// newVals block (+ 1)
-    (TurnOff block, True) -> grid V.// newVals block (\v -> if v == 0 then 0 else v - 1)
-    (Toggle block, True) -> grid V.// newVals block (+ 2)
+    (TurnOn block, False) -> updateGrid block (const 1)
+    (TurnOff block, False) -> updateGrid block (const 0)
+    (Toggle block, False) -> updateGrid block (\v -> if v == 0 then 1 else 0)
+    (TurnOn block, True) -> updateGrid block (+ 1)
+    (TurnOff block, True) -> updateGrid block (\v -> if v == 0 then 0 else v - 1)
+    (Toggle block, True) -> updateGrid block (+ 2)
   where
-    idxAndVal x y fn = (x * 1000 + y, fn $ grid V.! (x * 1000 + y))
-    newVals (Coord x1 y1, Coord x2 y2) fn = [idxAndVal x y fn | x <- [x1 .. x2], y <- [y1 .. y2]]
+    updateGrid (Coord x1 y1, Coord x2 y2) fn =
+      forM_ [y1 .. y2] $ \y ->
+        forM_ [x1 .. x2] $ \x -> do
+          let idx = x * numCols + y
+          val <- MV.read grid idx
+          MV.write grid idx (fn val)
