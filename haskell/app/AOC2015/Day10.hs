ccb6aab25149207f9711bbafda35d2ead0308f10
Bhavesh Sooka <bsooka@gmail.com>
2025-08-04 22:50:40 +0200

    small performance boost

--- haskell/app/AOC2015/Day10.hs
+++ haskell/app/AOC2015/Day10.hs
@@ -5,15 +5,16 @@ module AOC2015.Day10
 where
 
 import Data.Text qualified as T
+import Data.List (group)
 
 part1 :: T.Text -> Int
-part1 input = T.length $ repeatDescribe 40 input
+part1 input = length $ repeatDescribe 40 $ T.unpack input
 
 part2 :: T.Text -> Int
-part2 input = T.length $ repeatDescribe 50 input
+part2 input = length $ repeatDescribe 50 $ T.unpack input
 
-repeatDescribe :: Int -> T.Text -> T.Text
+repeatDescribe :: Int -> String -> String
 repeatDescribe n s = if n == 0 then s else repeatDescribe (n - 1) $ describeNumber s
   where
-    describeNumber s' = T.concat $ transform <$> T.group s'
-    transform s' = T.pack (show $ T.length s') <> T.take 1 s'
+    describeNumber s' = concatMap transform (group s')
+    transform s' = show (length s') <> take 1 s'
