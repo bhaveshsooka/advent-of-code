17576d5c63d0a05cc638e22d2f2cf7c511333d8a
Bhavesh Sooka <bsooka@gmail.com>
2025-11-14 11:56:17 +0200

    some cleanup

--- haskell/app/AOC2024/Day25.hs
+++ haskell/app/AOC2024/Day25.hs
@@ -19,10 +19,9 @@ part2 _input = 0
 parseKeysAndLocks :: T.Text -> ([[Int]], [[Int]])
 parseKeysAndLocks input = (makeKeyOrLock <$> keyGrids, makeKeyOrLock <$> lockGrids)
   where
-    makeKeyOrLock grid = foldl (zipWith (+)) (replicate 5 (-1)) (intGrid grid)
-    (keyGrids, lockGrids) = foldl keysAndLocksFold ([], []) grids
-    grids = T.splitOn (T.pack "\n") <$> T.splitOn (T.pack "\n\n") input
-    keysAndLocksFold (ks, lks) x = if T.unpack (last x) == "#####" then (ks ++ [x], lks) else (ks, lks ++ [x])
-    intGrid = foldl (\acc x -> acc ++ [intLineFold [] x]) []
+    makeKeyOrLock grid = foldl (zipWith (+)) (replicate 5 (-1)) (intLineFold [] <$> grid)
     intLineFold acc gridLine = acc ++ (toNum <$> T.unpack gridLine)
+    (keyGrids, lockGrids) = foldr keysAndLocksFold ([], []) grids
+    grids = T.splitOn (T.pack "\n") <$> T.splitOn (T.pack "\n\n") input
+    keysAndLocksFold x (ks, lks) = if T.unpack (last x) == "#####" then (x : ks, lks) else (ks, x : lks)
     toNum x = (if x == '#' then 1 else 0) :: Int
