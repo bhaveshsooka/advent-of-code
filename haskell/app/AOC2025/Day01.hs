815986a2398afabf99e730a6641dd18c06bdab62
Bhavesh Sooka <bsooka@gmail.com>
2025-12-01 22:07:43 +0200

    feat: haskell 2025 day 1  p2 - no words

--- haskell/app/AOC2025/Day01.hs
+++ haskell/app/AOC2025/Day01.hs
@@ -5,15 +5,14 @@ module AOC2025.Day01
 where
 
 import Data.Text qualified as T
-import Debug.Trace (trace)
 import Text.Parsec qualified as P
 import Util.ParseUtils (parseAoCInput)
 
 part1 :: T.Text -> Int
 part1 input = fst $ count0s (parseRotations input) (0, 50)
 
--- part2 :: T.Text -> Int
-part2 input = countClicks (parseRotations input) (0, 50)
+part2 :: T.Text -> Int
+part2 input = fst $ countClicks (parseRotations input) (0, 50)
 
 data Rotation = L Int | R Int deriving (Show)
 
@@ -22,18 +21,22 @@ countClicks [] acc = acc
 countClicks (x : xs) (hits, dial) =
   case x of
     R rv ->
-      case dial + rv == 100  
-      if 
-        then countClicks xs (hits + 1, 0)
-        else 
-    L rv -> undefined
-  -- case (currentVal == 0, newVal == 0) of
-  --   (False, False) -> countClicks xs (count + abs (newVal `div` 100), newVal `mod` 100)
-  --   (False, True) -> countClicks xs (count + 1 + (rVal `div` 100), newVal `mod` 100)
-  --   (True, _) -> countClicks xs (count + (rVal `div` 100), newVal `mod` 100)
-  -- where
-  --   rVal = case x of L rv -> rv; R rv -> rv
-  --   newVal = case x of L rv -> currentVal - rv; R rv -> currentVal + rv
+      if (dial + rv) < 100
+        then countClicks xs (hits, dial + rv)
+        else
+          if (dial + rv) == 100
+            then countClicks xs (hits + 1, 0)
+            else countClicks xs (hits + (dial + rv) `div` 100, (dial + rv) `mod` 100)
+    L rv ->
+      if (dial - rv) > 0
+        then countClicks xs (hits, dial - rv)
+        else
+          if (dial - rv) == 0
+            then countClicks xs (hits + 1, 0)
+            else
+              if dial == 0
+                then countClicks xs (hits + rv `div` 100, (dial - rv) `mod` 100)
+                else countClicks xs (hits + 1 + abs ((dial - rv) `quot` 100), (dial - rv) `mod` 100)
 
 count0s :: [Rotation] -> (Int, Int) -> (Int, Int)
 count0s [] acc = acc
