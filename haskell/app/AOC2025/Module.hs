e927575016f5b64ccb7f01ad2fc4726c7d1106b8
Bhavesh Sooka <bsooka@gmail.com>
2025-12-03 07:48:23 +0200

    feat: Haskell 2025 day 3 parts 1 & 2

--- haskell/app/AOC2025/Module.hs
+++ haskell/app/AOC2025/Module.hs
@@ -5,6 +5,7 @@ where
 
 import AOC2025.Day01 qualified as Day01
 import AOC2025.Day02 qualified as Day02
+import AOC2025.Day03 qualified as Day03
 import Model (AOCDayImpl (AOCNoDay, AOCPartsFunction))
 
 getParts :: Int -> AOCDayImpl
@@ -12,4 +13,5 @@ getParts day =
   case day of
     01 -> AOCPartsFunction Day01.part1 Day01.part2
     02 -> AOCPartsFunction Day02.part1 Day02.part2
+    03 -> AOCPartsFunction Day03.part1 Day03.part2
     _ -> AOCNoDay
